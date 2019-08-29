21ee00b1f5e5991b714d619bf7c6399baa31b3d8
Sarina <sbi@touchlogic.dk>
2019-08-29 14:18:51 +0200

    Initial commit

---
+++ GMTestPod/GMTestPod.h
@@ -0,0 +1,19 @@
+//
+//  GMTestPod.h
+//  GMTestPod
+//
+//  Created by Sarina Bibæk on 29/08/2019.
+//  Copyright © 2019 Sarina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GMTestPod.
+FOUNDATION_EXPORT double GMTestPodVersionNumber;
+
+//! Project version string for GMTestPod.
+FOUNDATION_EXPORT const unsigned char GMTestPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GMTestPod/PublicHeader.h>
+
+
